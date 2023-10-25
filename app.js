95eb80075e036138ef6a5aa4c81d280860c0ca5e
lzq603 <iv2013@qq.com>
2023-10-25 14:15:31 +0800

    添加多小程序支持

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ App({
       success: res => {
         console.log(res);
         wx.request({
-          url: that.globalData.host + '/login/?code=' + res.code,
+          url: that.globalData.host + '/login/?code=' + res.code + '&appid=' + that.globalData.appid + '&appsecret=' + that.globalData.appsecret,
           success(res){
             console.log(res);
             wx.setStorage({key: 'times', data:res.data.data.times})
@@ -26,6 +26,8 @@ App({
     // host: 'http://82.157.22.44:8000/chat',
     // ws_host: 'ws://82.157.22.44:8000/chat',
     host: 'https://chatwx.crazystone.work/chat',
-    ws_host: 'wss://chatwx.crazystone.work/chat'
+    ws_host: 'wss://chatwx.crazystone.work/chat',
+    appid: 'wx56b7c4105309a84f',
+    appsecret: 'b339a20b55090910ab79615121f6952f'
   }
 })
